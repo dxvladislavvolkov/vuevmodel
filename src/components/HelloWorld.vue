2f9ad249c3d807fda40d30c8739eebd5fc1ecaa5
dxvladislavvolkov <vladislav.volkov@devexpress.com>
2022-06-30 17:14:16 +0400

    Init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="content-block">
+    <input
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    modelValue: String
+  },
+  emits: ['update:modelValue']
+}
+</script>
